863c801cdb04ccd28092ba68f5907f30c1f359dc
ck <ck@gps-e.com>
2016-03-13 12:09:23 +0800

    improve code for log.h

--- player-win32/ffplayer/log.h
+++ player-win32/ffplayer/log.h
@@ -4,9 +4,17 @@
 // 包含头文件
 #include "stdefine.h"
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 /* 函数声明 */
 void log_init  (TCHAR *file);
 void log_done  (void);
 void log_printf(TCHAR *format, ...);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif
