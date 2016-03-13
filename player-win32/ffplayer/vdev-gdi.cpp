d33c7c296addeda446762d2314daceae84cf2e93
ck <ck@gps-e.com>
2016-03-13 20:15:24 +0800

    fix background refresh issue when resize window.

--- player-win32/ffplayer/vdev-gdi.cpp
+++ player-win32/ffplayer/vdev-gdi.cpp
@@ -203,10 +203,20 @@ void vdev_gdi_post(void *ctxt, int64_t pts)
 
 void vdev_gdi_setrect(void *ctxt, int x, int y, int w, int h)
 {
-    ((DEVGDICTXT*)ctxt)->x = x;
-    ((DEVGDICTXT*)ctxt)->y = y;
-    ((DEVGDICTXT*)ctxt)->w = w;
-    ((DEVGDICTXT*)ctxt)->h = h;
+    DEVGDICTXT *c = (DEVGDICTXT*)ctxt;
+    c->x = x; c->y = y;
+    c->w = w; c->h = h;
+
+    RECT rtwin, rect1, rect2, rect3, rect4;
+    GetClientRect(c->hwnd, &rtwin);
+    rect1.left = 0;   rect1.top = 0;   rect1.right = rtwin.right; rect1.bottom = y;
+    rect2.left = 0;   rect2.top = y;   rect2.right = x;           rect2.bottom = y+h;
+    rect3.left = x+w; rect3.top = y;   rect3.right = rtwin.right; rect3.bottom = y+h;
+    rect4.left = 0;   rect4.top = y+h; rect4.right = rtwin.right; rect4.bottom = rtwin.bottom;
+    InvalidateRect(c->hwnd, &rect1, FALSE);
+    InvalidateRect(c->hwnd, &rect2, FALSE);
+    InvalidateRect(c->hwnd, &rect3, FALSE);
+    InvalidateRect(c->hwnd, &rect4, FALSE);
 }
 
 void vdev_gdi_pause(void *ctxt, BOOL pause)
