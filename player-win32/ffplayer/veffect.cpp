b0918dc5cba27b1de32aa81e321ec6be19115d79
chenk <chenk@apical.com.cn>
2016-08-16 19:30:54 +0800

    beautify the visual effect of spectrum.

--- player-win32/ffplayer/veffect.cpp
+++ player-win32/ffplayer/veffect.cpp
@@ -5,6 +5,10 @@
 #include "fft.h"
 #include "log.h"
 
+// 内部常量定义
+#define MAX_GRID_COUNT_X  64
+#define MAX_GRID_COUNT_Y  16
+
 // 内部类型定义
 typedef struct {
     HWND     hwnd;
@@ -12,8 +16,10 @@ typedef struct {
     int      h;
     HDC      hdcdst;
     HDC      hdcsrc;
-    HPEN     hpen;
+    HPEN     hpen0;
+    HPEN     hpen1;
     HBITMAP  hbmp;
+    HBITMAP  hfill;
     BYTE    *pbmp;
     int      stride;
     int      data_len;
@@ -22,21 +28,11 @@ typedef struct {
 } VEFFECT;
 
 // 内部函数实现
-static void draw_effect(VEFFECT *ve, int x, int y, int w, int h, float factor, float *sample, int n)
+static void resize_veffect_ifneeded(VEFFECT *ve, int w, int h)
 {
-    // create dc for ve
-    if (!ve->hdcsrc) {
-        ve->hdcsrc = CreateCompatibleDC(ve->hdcdst);
-    }
-
-    // create pen for ve
-    if (!ve->hpen) {
-        ve->hpen = CreatePen(PS_SOLID, 1, RGB(0, 255, 0));
-        SelectObject(ve->hdcsrc, ve->hpen);
-    }
-
-    // create bitmap for ve
-    if (!ve->hbmp || ve->w != w || ve->h != h) {
+    if (!ve->hbmp || ve->w != w || ve->h != h)
+    {
+        //++ re-create bitmap for draw buffer
         BITMAPINFO bmpinfo = {0};
         bmpinfo.bmiHeader.biSize        =  sizeof(BITMAPINFOHEADER);
         bmpinfo.bmiHeader.biWidth       =  w;
@@ -54,7 +50,45 @@ static void draw_effect(VEFFECT *ve, int x, int y, int w, int h, float factor, f
         ve->w      = w;
         ve->h      = h;
         ve->stride = bitmap.bmWidthBytes;
+        //-- re-create bitmap for draw buffer
+
+        //++ re-create bitmap for gradient fill
+        if (ve->hfill) DeleteObject(ve->hfill);
+        bmpinfo.bmiHeader.biSize        =  sizeof(BITMAPINFOHEADER);
+        bmpinfo.bmiHeader.biWidth       =  w / MAX_GRID_COUNT_X;
+        bmpinfo.bmiHeader.biHeight      = -h / MAX_GRID_COUNT_Y * MAX_GRID_COUNT_Y;
+        bmpinfo.bmiHeader.biPlanes      =  1;
+        bmpinfo.bmiHeader.biBitCount    =  32;
+        bmpinfo.bmiHeader.biCompression =  BI_RGB;
+        ve->hfill = CreateDIBSection(ve->hdcsrc, &bmpinfo, DIB_RGB_COLORS, NULL, NULL, 0);
+        HDC hdc   = CreateCompatibleDC(ve->hdcsrc);
+        SelectObject(hdc, ve->hfill);
+        TRIVERTEX     vert[2];
+        GRADIENT_RECT grect;
+        vert[0].x        = 0;
+        vert[0].y        = 0;
+        vert[0].Red      = 0xffff;
+        vert[0].Green    = 0xdddd;
+        vert[0].Blue     = 0x1111;
+        vert[0].Alpha    = 0x0000;
+        vert[1].x        = bmpinfo.bmiHeader.biWidth;
+        vert[1].y        =-bmpinfo.bmiHeader.biHeight;
+        vert[1].Red      = 0x8888;
+        vert[1].Green    = 0xffff;
+        vert[1].Blue     = 0x1111;
+        vert[1].Alpha    = 0x0000;
+        grect.UpperLeft  = 0;
+        grect.LowerRight = 1;
+        GradientFill(hdc, vert, 2, &grect, 1, GRADIENT_FILL_RECT_V);
+        DeleteDC(hdc);
+        //-- re-create bitmap for gradient fill
     }
+}
+
+static void draw_waveform(VEFFECT *ve, int x, int y, int w, int h, float divisor, float *sample, int n)
+{
+    // resize veffect if needed
+    resize_veffect_ifneeded(ve, w, h);
 
     //++ draw visual effect
     int delta  = n / w > 1 ? n / w : 1;
@@ -64,15 +98,70 @@ static void draw_effect(VEFFECT *ve, int x, int y, int w, int h, float factor, f
     memset(ve->pbmp, 0, ve->stride * h);
 
     px = 0;
-    py = y + h - (int)(h * sample[0] / factor);
-    MoveToEx(ve->hdcsrc, px, py, NULL);
+    py = y + h - (int)(h * sample[0] / divisor);
+    SelectObject(ve->hdcsrc, ve->hpen0);
+    MoveToEx(ve->hdcsrc, px, py, NULL );
     for (i=delta; i<n; i+=delta) {
         px = x + w * i / n;
-        py = y + h - (int)(h * sample[i] / factor);
+        py = y + h - (int)(h * sample[i] / divisor);
         LineTo(ve->hdcsrc, px, py);
     }
     BitBlt(ve->hdcdst, x, y, w, h, ve->hdcsrc, 0, 0, SRCCOPY);
     //-- draw visual effect
+}
+
+static void draw_spectrum(VEFFECT *ve, int x, int y, int w, int h, float *sample, int n)
+{
+    int    amplitude;
+    int    gridw, gridh;
+    int    d = n / MAX_GRID_COUNT_X;
+    float *fsrc = sample;
+    int    i, j, tx, ty;
+    int    sw, sh, sx, sy;
+    HDC    hdc;
+
+    // resize veffect if needed
+    resize_veffect_ifneeded(ve, w, h);
+
+    // clear bitmap
+    memset(ve->pbmp, 0, ve->stride * h);
+
+    // calucate for grid
+    gridw = w / MAX_GRID_COUNT_X; if (gridw == 0) gridw = 1;
+    gridh = h / MAX_GRID_COUNT_Y; if (gridh == 0) gridh = 1;
+    sw = gridw * MAX_GRID_COUNT_X; sx = x + (w - sw) / 2;
+    sh = gridh * MAX_GRID_COUNT_Y; sy = y + (h - sh) / 2;
+
+    hdc = CreateCompatibleDC(ve->hdcsrc);
+    SelectObject(hdc       , ve->hfill);
+    SelectObject(ve->hdcsrc, ve->hpen1);
+
+    for (ty=sy; ty<=sy+sh; ty+=gridh) {
+        MoveToEx(ve->hdcsrc, sx, ty, NULL);
+        LineTo  (ve->hdcsrc, sx + sw, ty );
+    }
+
+    // calculate amplitude
+    for (i=0; i<MAX_GRID_COUNT_X; i++) {
+        amplitude = 0;
+        for (j=0; j<d; j++) {
+            amplitude += (int)(*fsrc++ * sh/0x100000);
+        }
+        amplitude /= d;
+        if (amplitude > sh) amplitude = sh;
+        tx = sx + (i + 0) * gridw;
+        ty = sy + sh - amplitude;
+        BitBlt(ve->hdcsrc, tx, ty, gridw, amplitude,
+               hdc, 0, sh - amplitude, SRCCOPY);
+    }
+
+    for (tx=sx; tx<=sx+sw; tx+=gridw) {
+        MoveToEx(ve->hdcsrc, tx, sy, NULL);
+        LineTo  (ve->hdcsrc, tx, sy + sh );
+    }
+
+    BitBlt(ve->hdcdst, x, y, w, h, ve->hdcsrc, 0, 0, SRCCOPY);
+    DeleteDC(hdc);
 }
 
 // 函数实现
@@ -84,6 +173,10 @@ void* veffect_create(void *surface)
     memset(ve, 0, sizeof(VEFFECT));
     ve->hwnd   = (HWND) surface;
     ve->hdcdst = GetDC((HWND)surface);
+    ve->hdcsrc = CreateCompatibleDC(ve->hdcdst);
+    ve->hpen0  = CreatePen(PS_SOLID, 1, RGB(0 , 255, 0 ));
+    ve->hpen1  = CreatePen(PS_SOLID, 1, RGB(32, 32 , 64));
+
     return ve;
 }
 
@@ -94,8 +187,10 @@ void veffect_destroy(void *ctxt)
     // for dc & object
     ReleaseDC(ve->hwnd, ve->hdcdst);
     DeleteDC (ve->hdcsrc);
-    DeleteObject(ve->hpen);
-    DeleteObject(ve->hbmp);
+    DeleteObject(ve->hpen0);
+    DeleteObject(ve->hpen1);
+    DeleteObject(ve->hfill);
+    DeleteObject(ve->hbmp );
 
     // free fft
     fft_free(ve->fft);
@@ -135,7 +230,7 @@ void veffect_render(void *ctxt, int x, int y, int w, int h, int type, void *buf,
                 fdst += 1;
                 ssrc += 2;
             }
-            draw_effect(ve, x, y, w, h, 0x10000, ve->data_buf, snum);
+            draw_waveform(ve, x, y, w, h, 0x10000, ve->data_buf, snum);
         }
         break;
     case VISUAL_EFFECT_SPECTRUM:
@@ -156,7 +251,8 @@ void veffect_render(void *ctxt, int x, int y, int w, int h, int type, void *buf,
                 fdst += 1;
                 fsrc += 2;
             }
-            draw_effect(ve, x, y, w, h, 0x100000, ve->data_buf, ve->data_len / 2);
+//          draw_waveform(ve, x, y, w, h, 0x100000, ve->data_buf, ve->data_len);
+            draw_spectrum(ve, x, y, w, h, ve->data_buf, ve->data_len * 47 / 128); // 44100 * 47 / 128 = 16K
         }
         break;
     }
