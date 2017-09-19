0c66c6969e00882f4dd26d260bea5ad36e4ec1d5
chenk <chenk@apical.com.cn>
2017-09-19 18:09:37 +0800

    improve code for seek operation.

--- src/ffplayer.cpp
+++ src/ffplayer.cpp
@@ -295,7 +295,7 @@ static void* video_decode_thread_proc(void *param)
                         }
                     }
                     //-- for seek operation
-                    else render_video(player->render, vframe);
+                    render_video(player->render, vframe);
                 } while (player->vfilter_graph && player->vfilter_enable);
             }
 
