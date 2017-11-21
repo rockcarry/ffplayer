4339e8b7ecb15450d67121be3fd0780f4bb4b320
chenk <chenk@apical.com.cn>
2017-11-21 17:00:14 +0800

    fix bug for vdev-cmn.cpp

--- src/vdev-cmn.cpp
+++ src/vdev-cmn.cpp
@@ -236,9 +236,9 @@ void vdev_handle_complete_and_avsync(void *ctxt)
         if (tickdiff - c->tickframe >  2) c->ticksleep--;
         if (tickdiff - c->tickframe < -2) c->ticksleep++;
         if (c->apts != -1 && c->vpts != -1) {
-            if      (avdiff >  500) c->ticksleep -= 5;
+                 if (avdiff >  500) c->ticksleep -= 5;
             else if (avdiff >  50 ) c->ticksleep -= 1;
-            else if (avdiff < -500) c->ticksleep -= 5;
+            else if (avdiff < -500) c->ticksleep += 5;
             else if (avdiff < -50 ) c->ticksleep += 1;
         }
         if (c->ticksleep < 0) c->ticksleep = 0;
