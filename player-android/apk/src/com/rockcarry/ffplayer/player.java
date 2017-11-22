ad97127b15978748d08d9566685d2f93b1f5fd2e
chenk <chenk@apical.com.cn>
2017-11-22 15:54:21 +0800

    improve code for player.java

--- player-android/apk/src/com/rockcarry/ffplayer/player.java
+++ player-android/apk/src/com/rockcarry/ffplayer/player.java
@@ -5,32 +5,21 @@ import android.os.Message;
 
 public final class player
 {
-    public static final int SCALING_MODE_SCALE_TO_WINDOW = 1;
-    public static final int SCALING_MODE_SCALE_CROP      = 2;
-    public static final int SCALING_MODE_NO_SCALE_CROP   = 3;
+    public static final int MSG_INIT_DONE           = (('I' << 24) | ('N' << 16) | ('I' << 8) | ('T' << 0));
+    public static final int MSG_PLAY_PROGRESS       = (('R' << 24) | ('U' << 16) | ('N' << 8) | (' ' << 0));
 
-    public static final int MSG_PLAY_PROGRESS            = (('R' << 24) | ('U' << 16) | ('N' << 8) | (' ' << 0));
-    public static final int MSG_PLAY_COMPLETED           = (('E' << 24) | ('N' << 16) | ('D' << 8) | (' ' << 0));
-
-    public static final int PARAM_MEDIA_DURATION         = 0x1000 + 0;
-    public static final int PARAM_MEDIA_POSITION         = 0x1000 + 1;
-    public static final int PARAM_VIDEO_WIDTH            = 0x1000 + 2;
-    public static final int PARAM_VIDEO_HEIGHT           = 0x1000 + 3;
-//  public static final int PARAM_VIDEO_MODE             = 0x1000 + 4;
-    public static final int PARAM_AUDIO_VOLUME           = 0x1000 + 5;
-    public static final int PARAM_PLAY_SPEED             = 0x1000 + 6;
-    public static final int PARAM_DECODE_THREAD_COUNT    = 0x1000 + 7;
-//  public static final int PARAM_VISUAL_EFFECT          = 0x1000 + 8;
-    public static final int PARAM_AVSYNC_TIME_DIFF       = 0x1000 + 9;
-//  public static final int PARAM_PLAYER_CALLBACK        = 0x1000 +10;
-    public static final int PARAM_AUDIO_STREAM_TOTAL     = 0x1000 +11;
-    public static final int PARAM_VIDEO_STREAM_TOTAL     = 0x1000 +12;
-    public static final int PARAM_SUBTITLE_STREAM_TOTAL  = 0x1000 +13;
-    public static final int PARAM_AUDIO_STREAM_CUR       = 0x1000 +14;
-    public static final int PARAM_VIDEO_STREAM_CUR       = 0x1000 +15;
-    public static final int PARAM_SUBTITLE_STREAM_CUR    = 0x1000 +16;
-    public static final int PARAM_AFILTER_ENABLE         = 0x1000 +17;
-    public static final int PARAM_VFILTER_ENABLE         = 0x1000 +18;
+    public static final int PARAM_MEDIA_DURATION    = 0x1000 + 0;
+    public static final int PARAM_MEDIA_POSITION    = 0x1000 + 1;
+    public static final int PARAM_VIDEO_WIDTH       = 0x1000 + 2;
+    public static final int PARAM_VIDEO_HEIGHT      = 0x1000 + 3;
+//  public static final int PARAM_VIDEO_MODE        = 0x1000 + 4;
+    public static final int PARAM_AUDIO_VOLUME      = 0x1000 + 5;
+    public static final int PARAM_PLAY_SPEED        = 0x1000 + 6;
+//  public static final int PARAM_VISUAL_EFFECT     = 0x1000 + 7;
+    public static final int PARAM_AVSYNC_TIME_DIFF  = 0x1000 + 8;
+//  public static final int PARAM_PLAYER_CALLBACK   = 0x1000 + 9;
+    public static final int PARAM_AFILTER_ENABLE    = 0x1000 +10;
+    public static final int PARAM_VFILTER_ENABLE    = 0x1000 +11;
 
     private long m_hPlayer = 0;
     public boolean open(String url) {
@@ -47,7 +36,6 @@ public final class player
     public long getParam(int id)             { return nativeGetParam(m_hPlayer, id); }
     public void setDisplaySurface(Object surface) { nativeSetDisplaySurface(m_hPlayer, surface); }
     public void setDisplayTexture(Object texture) { nativeSetDisplayTexture(m_hPlayer, texture); }
-    public void setDispScaleMode (int    mode   ) { nativeSetDispScaleMode(m_hPlayer, mode    ); }
 
     public void setPlayerMsgHandler(Handler h) {
         mPlayerMsgHandler = h;
@@ -76,8 +64,7 @@ public final class player
     private static native long nativeGetParam(long hplayer, int id);
     private static native void nativeSetDisplaySurface(long hplayer, Object surf);
     private static native void nativeSetDisplayTexture(long hplayer, Object text);
-    private static native void nativeSetDispScaleMode(long hplayer, int mode  );
-    private static native void nativeEnableCallback  (long hplayer, int enable);
+    private static native void nativeEnableCallback   (long hplayer, int enable );
 
     static {
         System.loadLibrary("ffplayer_jni");
