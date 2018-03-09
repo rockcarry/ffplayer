6b72d5a79b98b4df6734ccbe3d760db616d55b4f
chenk <chenk@apical.com.cn>
2018-03-09 16:59:25 +0800

    修改 vdev-android.cpp 代码，解决播放完成消息发送导致崩溃的问题

--- src/vdev-android.cpp
+++ src/vdev-android.cpp
@@ -77,6 +77,7 @@ void* vdev_android_create(void *surface, int bufnum, int w, int h, int frate)
 
     // init vdev context
     ctxt->pixfmt    = android_pixfmt_to_ffmpeg_pixfmt(DEF_WIN_PIX_FMT);
+    ctxt->surface   = surface;
     ctxt->w         = w ? w : 1;
     ctxt->h         = h ? h : 1;
     ctxt->sw        = w ? w : 1;
